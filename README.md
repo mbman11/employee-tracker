ec3ac9f3e39b0592475c76bae54bb7f1b3451ecf
mbman11 <m.behrmann11@gmail.com>
2023-09-12 23:52:30 -0600

    added and created functions for inquirer to return after user input, also added commented out code which adds departments to table

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # employee-tracker
+
+## This employee tracker uses inquirer to accept user input to browse an sql database
+
+## 
--- server.js
+++ server.js
@@ -1,37 +1,116 @@
 const inquirer = require('inquirer');
-// const express = require('express');
-// const app = express();
 const db = require('./db/connection')
 
-// app.use(express.urlencoded({ extended: false }));
-// app.use(express.json());
+const userInput = '';
 
-
-const menu = inquirer
-  .prompt([
+function runMenu () {
+  inquirer.prompt([
     {
       type: 'list',
       message: 'What would you like to do?',
       name: 'options',
-      choices: ['view all departments','view all roles', 'view all employees'],
+      choices: ['view all departments','view all roles', 'view all employees','add department','add role','add employee','quit'],
     },
   ])
-.then((response)=>{
+  .then((response)=>{
   if(response.options === 'view all departments'){
     db.query('SELECT * FROM departments', function (err, results) {
       console.table(results);
+      return runMenu();
     });
   }
   if(response.options === 'view all roles'){
     db.query('SELECT * FROM roles', function (err, results) {
       console.table(results);
+      return runMenu();
     });
   }
   if(response.options === 'view all employees'){
     db.query('SELECT * FROM employees', function (err, results) {
       console.table(results);
+      return runMenu();
     });
   }
+  // if(response.options == 'add department'){
+  //   inquirer.prompt([
+  //     {
+  //     type:'input',
+  //     message:'Please type a department',
+  //     name:'addDepartment',
+  //   }]).then((response)=>{
+  //     if(response.addDepartment == 'sales'){
+  //       const sql = `INSERT INTO departments(name) VALUES('sales')`
+  //       db.query(sql, function (err,results){
+  //         console.table(results)
+  //         return runMenu();
+  //       })
+  //     }
+  //   })
+  // }
+  if(response.options === 'quit'){
+    console.log('Application Ended. Have A Great Day!')
+    process.exit();
+  }
 })
+}
+
+
+
+
+inquirer.prompt([
+    {
+      type: 'list',
+      message: 'What would you like to do?',
+      name: 'options',
+      choices: ['view all departments','view all roles', 'view all employees','add department','add role','add employee','quit'],
+    },
+  ])  
+  .then((response)=>{
+  if(response.options === 'view all departments'){
+    db.query('SELECT * FROM departments', function (err, results) {
+      console.table(results);
+      if(response){
+        runMenu();
+      }
+    });
+  }
+  if(response.options === 'view all roles'){
+    db.query('SELECT * FROM roles', function (err, results) {
+      console.table(results);
+      if(response){
+        runMenu();
+      }
+    });
+  }
+  if(response.options === 'view all employees'){
+    db.query('SELECT * FROM employees', function (err, results) {
+      console.table(results);
+      if(response){
+        runMenu();
+      }
+    });
+  }
+  // if(response.options == 'add department'){
+  //   inquirer.prompt([
+  //     {
+  //     type:'input',
+  //     message:'Please type a department',
+  //     name:'addDepartment',
+  //   }]).then((response)=>{
+  //     if(response.addDepartment == 'sales'){
+  //       db.query(userInput, function (err,results){
+  //         console.table(results)
+  //         return runMenu();
+  //       })
+  //     }
+  //   })
+  // }
+  if(response.options === 'quit'){
+    console.log('Application Ended. Have A Great Day!')
+    process.exit();
+  }
+});
+
+
 
 
